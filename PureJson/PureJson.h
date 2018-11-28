f587cff26ec91342f52ffc06d0f8e1b8381f19eb
Matthew <matthew.mcdade13@gmail.com>
2018-11-28 02:13:59 +0000

    added a way to dot into nested objects (still needs more testing)

--- PureJson/PureJson.h
+++ PureJson/PureJson.h
@@ -77,63 +77,63 @@ enum pj_ValueType
 };
 
 /* Object Create/Delete */
-EXTERN_C pj_Object* pj_createObj();
-EXTERN_C void pj_deleteObj(pj_Object* json);
+EXTERN_C  pj_Object* pj_createObj();
+EXTERN_C  void pj_deleteObj(pj_Object* json);
 
 /* Array Create/Delete */
-EXTERN_C pj_Array* pj_createArray();
-EXTERN_C void pj_deleteArray(pj_Array* array);
+EXTERN_C  pj_Array* pj_createArray();
+EXTERN_C  void pj_deleteArray(pj_Array* array);
 
-EXTERN_C void pj_deleteString(char* jsonString);
+EXTERN_C  void pj_deleteString(char* jsonString);
 
 /* Object/Array Parsers */
-EXTERN_C pj_Object* pj_parseObj(const char* raw);
-EXTERN_C pj_Array* pj_parseArray(const char* raw);
+EXTERN_C  pj_Object* pj_parseObj(const char* raw);
+EXTERN_C  pj_Array* pj_parseArray(const char* raw);
 
-EXTERN_C char* pj_arrayToString(pj_Array* array, pj_boolean isPretty);
-EXTERN_C pj_boolean pj_arrayToFile(pj_Array* array, pj_boolean isPretty, const char* fileName);
-EXTERN_C char* pj_objToString(pj_Object* obj, pj_boolean isPretty);
-EXTERN_C pj_boolean pj_objToFile(pj_Object* obj, pj_boolean isPretty, const char* fileName);
+EXTERN_C  char* pj_arrayToString(pj_Array* array, pj_boolean isPretty);
+EXTERN_C  pj_boolean pj_arrayToFile(pj_Array* array, pj_boolean isPretty, const char* fileName);
+EXTERN_C  char* pj_objToString(pj_Object* obj, pj_boolean isPretty);
+EXTERN_C  pj_boolean pj_objToFile(pj_Object* obj, pj_boolean isPretty, const char* fileName);
 
 /* Object Get */
-EXTERN_C double pj_objGetNum(pj_Object* json, const char* propName);
-EXTERN_C pj_boolean pj_objGetBool(pj_Object* json, const char* propName);
-EXTERN_C const char* pj_objGetString(pj_Object* json, const char* propName);
-EXTERN_C pj_Array* pj_objGetArray(pj_Object* json, const char* propName);
-EXTERN_C pj_Object* pj_objGetObj(pj_Object* json, const char* propName);
+EXTERN_C  double pj_objGetNum(pj_Object* json, const char* propName);
+EXTERN_C  pj_boolean pj_objGetBool(pj_Object* json, const char* propName);
+EXTERN_C  const char* pj_objGetString(pj_Object* json, const char* propName);
+EXTERN_C  pj_Array* pj_objGetArray(pj_Object* json, const char* propName);
+EXTERN_C  pj_Object* pj_objGetObj(pj_Object* json, const char* propName);
 
 /* Array Get */
-EXTERN_C double pj_arrayGetNum(pj_Array* array, size_t index);
-EXTERN_C pj_boolean pj_arrayGetBool(pj_Array* array, size_t index);
-EXTERN_C const char* pj_arrayGetString(pj_Array* array, size_t index);
-EXTERN_C pj_Array* pj_arrayGetArray(pj_Array* array, size_t index);
-EXTERN_C pj_Object* pj_arrayGetObj(pj_Array* array, size_t index);
+EXTERN_C  double pj_arrayGetNum(pj_Array* array, size_t index);
+EXTERN_C  pj_boolean pj_arrayGetBool(pj_Array* array, size_t index);
+EXTERN_C  const char* pj_arrayGetString(pj_Array* array, size_t index);
+EXTERN_C  pj_Array* pj_arrayGetArray(pj_Array* array, size_t index);
+EXTERN_C  pj_Object* pj_arrayGetObj(pj_Array* array, size_t index);
 
 /* Array Add */
-EXTERN_C void pj_arrayAddNum(pj_Array* array, double num);
-EXTERN_C void pj_arrayAddBool(pj_Array* array, pj_boolean boolean);
-EXTERN_C void pj_arrayAddString(pj_Array* array, const char* str);
-EXTERN_C void pj_arrayAddArray(pj_Array* array, pj_Array* other);
-EXTERN_C void pj_arrayAddObj(pj_Array* array, pj_Object* obj);
-EXTERN_C void pj_arrayAddNull(pj_Array* array);
+EXTERN_C  void pj_arrayAddNum(pj_Array* array, double num);
+EXTERN_C  void pj_arrayAddBool(pj_Array* array, pj_boolean boolean);
+EXTERN_C  void pj_arrayAddString(pj_Array* array, const char* str);
+EXTERN_C  void pj_arrayAddArray(pj_Array* array, pj_Array* other);
+EXTERN_C  void pj_arrayAddObj(pj_Array* array, pj_Object* obj);
+EXTERN_C  void pj_arrayAddNull(pj_Array* array);
 
 /* Object Set */
-EXTERN_C void pj_objSetNum(pj_Object* obj, const char* propName, double num);
-EXTERN_C void pj_objSetBool(pj_Object* obj, const char* propName, pj_boolean boolean);
-EXTERN_C void pj_objSetString(pj_Object* obj, const char* propName, const char* str);
-EXTERN_C void pj_objSetArray(pj_Object* obj, const char* propName, pj_Array* array);
-EXTERN_C void pj_objSetObj(pj_Object* obj, const char* propName, pj_Object* other);
-EXTERN_C void pj_objSetNull(pj_Object* obj, const char* propName);
+EXTERN_C  void pj_objSetNum(pj_Object* obj, const char* propName, double num);
+EXTERN_C  void pj_objSetBool(pj_Object* obj, const char* propName, pj_boolean boolean);
+EXTERN_C  void pj_objSetString(pj_Object* obj, const char* propName, const char* str);
+EXTERN_C  void pj_objSetArray(pj_Object* obj, const char* propName, pj_Array* array);
+EXTERN_C  void pj_objSetObj(pj_Object* obj, const char* propName, pj_Object* other);
+EXTERN_C  void pj_objSetNull(pj_Object* obj, const char* propName);
 
 /* Value Inspection */
-EXTERN_C pj_ValueType pj_getObjPropType(pj_Object* obj, const char* propName);
-EXTERN_C pj_ValueType pj_getArrayElemType(pj_Array* array, size_t index);
-EXTERN_C pj_boolean pj_isArrayElemOfType(pj_Array* array, size_t index, pj_ValueType type);
-EXTERN_C pj_boolean pj_isObjPropOfType(pj_Object* obj, const char* propName, pj_ValueType type);
+EXTERN_C  pj_ValueType pj_getObjPropType(pj_Object* obj, const char* propName);
+EXTERN_C  pj_ValueType pj_getArrayElemType(pj_Array* array, size_t index);
+EXTERN_C  pj_boolean pj_isArrayElemOfType(pj_Array* array, size_t index, pj_ValueType type);
+EXTERN_C  pj_boolean pj_isObjPropOfType(pj_Object* obj, const char* propName, pj_ValueType type);
 
 /* Iteration */
-EXTERN_C void pj_objForEachKey(pj_Object* obj, void(*callback)(pj_Object*, const char*));
-EXTERN_C size_t pj_getArraySize(pj_Array* array);
+EXTERN_C  void pj_objForEachKey(pj_Object* obj, void(*callback)(pj_Object*, const char*));
+EXTERN_C  size_t pj_getArraySize(pj_Array* array);
 
 
 #if defined(PURE_JSON_IMPLEMENTATION)
@@ -601,7 +601,23 @@ T getValueOfType(JsonVal& val, T failVal)
 template<typename T, pj_ValueType valType>
 T getObjectValue(pj_Object* json, const char* propName, T failVal = 0)
 {
-	if (JsonProp* prop = findProp(*json, propName))
+	std::string name{ propName };
+
+	{
+		static const std::string delim = ".";
+		if (name.find(".") != std::string::npos)
+		{
+			size_t pos = name.find(".");
+			std::string prop = name.substr(0, pos);
+			JsonProp* jprop = findProp(*json, prop.c_str());
+			assert(jprop->val.type == PJ_VALUE_OBJ);
+			pj_Object* obj = jprop->val.obj;
+			name.erase(0, pos + delim.length());
+			return getObjectValue<T, valType>(obj, name.c_str(), failVal);
+		}
+	}
+
+	if (JsonProp* prop = findProp(*json, name.c_str()))
 	{
 		if (prop->val.type == PJ_VALUE_NULL) return failVal;
 
